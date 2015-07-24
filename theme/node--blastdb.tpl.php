99733929856638bef39567ce4a12c520b016292c
E.Cannon <ekcannon@iastsate.edu>
2015-07-24 13:57:56 -0500

    Restored linkout functionality

--- theme/node--blastdb.tpl.php
+++ theme/node--blastdb.tpl.php
@@ -99,6 +99,10 @@
       <tr><th>Human-Readable Name</th><td><?php print $node->db_name; ?></td></tr>
       <tr><th>Database Path</th><td><?php print $node->db_path; ?></td></tr>
       <tr><th>Database Type</th><td><?php print $node->db_dbtype; ?></td></tr>
+      <tr><th>FASTA Header Format</th><td><?php print $node->linkout->regex_type; ?></td></tr>
+      <tr><th>External Database</th><td><?php print $node->linkout->db_id; ?></td></tr>
+      <tr><th>Head RegEx</th><td><?php print $node->linkout->regex; ?></td></tr>
+      <tr><th>Link-out Type</th><td><?php print $node->linkout->type; ?></td></tr>
     </table>
 
     <?php
