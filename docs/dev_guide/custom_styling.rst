f9edf7a7b03f0cad36c994ec7da5034e3cddea68
Reynold Tan <reynold@reynolds-imac.gateway.sasktel.net>
2021-05-31 15:08:25 -0600

    Uses tripal_blast instead of blast_ui as the module name

--- docs/dev_guide/custom_styling.rst
+++
@@ -1,6 +0,0 @@
-Custom Styling
-===============
-
-The BLAST module forms can be styled using CSS stylesheets in your own theme. By default it will use the default form themeing provided by your particular Drupal site allowing it to feel consistent with the look-and-feel of your Tripal site without customization being needed.
-
-Additionally, the results page, waiting pages and the alignment section of the results page have their own template files (``blast_report.tpl.php``, ``blast_report_pending.tpl.php``, and ``blast_report_alignment_row.tpl.php``, respectively) which can easily be overridden in your own theme providing complete control over the look of the BLAST results.
---
+++ src/Controller/TripalBlastUI.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file 
+ * This route is for single all-in-one BLAST user interface.
+ * Choices of query type, protein or nucleotide, outlined in this page.
+ */
+
+namespace Drupal\tripal_blast\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Defines TripalBlastUI class.
+ * 
+ */
+class TripalBlastUI extends ControllerBase {
+  /**
+   * Returns a render-able array to create Tripal BLAST UI elements.
+   * A list of variables (context links presented in the interface) is used
+   * and is defined in the hook_theme implementation of this module.
+   * @see hook_theme in tripal_blast.module.
+   */
+  public function content() {
+    return [
+      // Tripal BLAST UI page theme.
+      '#theme' => 'theme-tripal-blast-ui'
+    ];  
+  }    
+}
