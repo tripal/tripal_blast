bdc97a8c1be592a3b9fa5aac60c6d0039cf4392a
Reynold Tan <reynold@reynolds-imac.gateway.sasktel.net>
2021-06-14 16:24:59 -0600

    BLAST programs pages - AJAX in FASTA field

--- src/Form/TripalBlastForm.php
+++ src/Form/TripalBlastForm.php
@@ -8,6 +8,8 @@ namespace Drupal\tripal_blast\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\ReplaceCommand;
 
 /**
  * Define Tripal BLAST form.
@@ -25,6 +27,9 @@ class TripalBlastForm extends FormBase {
    * Build form.
    */
   public function buildForm(array $form, FormStateInterface $form_state, $query = NULL, $program = NULL) {    
+    // Attach library - JS and Style.
+    $form['#attached']['library'][] = 'tripal_blast/tripal-blast-programs';
+
     // Add a warning, if need be (to be used for temporary message like down-for-maintenance).
     $config_warning_text = \Drupal::config('tripal_blast.settings')
       ->get('tripal_blast_config_notification.warning_text');
@@ -71,16 +76,58 @@ class TripalBlastForm extends FormBase {
       '#value' => $program_name
     ];
 
+    // # FIELDSET/DETAILS: MAIN.
     $form['new_blast'] = [
       '#type' => 'details',
       '#title' => $this->t('Request a New BLAST'),
       '#open' => TRUE,
     ];
+      
+      $form['new_blast']['query'] = [
+        '#type' => 'details',
+        '#title' => $this->t('Enter %type Query Sequence', ['%type' => $type]),
+        '#open' => TRUE,
+        '#description' => $this->t('Enter one or more queries in the top text box or 
+          use the browse button to upload a file from your local disk. The file 
+          may contain a single sequence or a list of sequences. In both cases, 
+          the data must be in <a href="@formaturl">FASTA format</a>.', 
+          ['@formaturl' => 'http://www.ncbi.nlm.nih.gov/BLAST/blastcgihelp.shtml']),
+      ];
+      
+        //
+        // # FIELD: SHOW EXAMPLE
+        // Checkbox to show an example.
+        $form['new_blast']['query']['fld_checkbox_example_sequence'] = [
+          '#type' => 'checkbox',
+          '#title' => $this->t('Show an Example Sequence'),
+          '#ajax' => [
+            'callback' => '::ajaxShowExampleSequenceCallback',
+            'wrapper'  => 'wrapper-ajax-fasta-textarea',
+            'method'   => 'replace',
+            'effect'   => 'fade',
+            'progress' => 'throbber',
+            'message'  => ''
+          ],
+          '#prefix' => '<div id="wrapper-checkbox-example-sequence">',
+          '#suffix' => '</div>',
+        ];
+     
+        //
+        // # FIELD: FASTA
+        // Textfield for submitting a mult-FASTA query
+        $form['new_blast']['query']['fld_text_fasta'] = [
+          '#type' => 'textarea',
+          '#title' => $this->t('Enter FASTA sequence(s)'),
+          '#description' => $this->t('Enter query sequence(s) in the text area.'),
+          // '#default_value' => $defaults['FASTA'],
+          '#prefix' => '<div id="wrapper-ajax-fasta-textarea">',
+          '#suffix' => '</div>',
+        ];
 
 
+      
 
-
-
+      
 
 
 
@@ -107,5 +154,20 @@ class TripalBlastForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     
+  }
+
+  /**
+   * AJAX callback, update FASTA text field to contain
+   * example FASTA sequence.
+   */
+  public function ajaxShowExampleSequenceCallback(array &$form, FormStateInterface $form_state) {
+    $fld_name_show_example = 'fld_checkbox_example_sequence';
+    $fld_value_show_example = $form_state->getValue($fld_name_show_example);
+
+    if ($fld_value_show_example) {
+      $form['new_blast']['query']['fld_text_fasta']['#value'] = 1;
+    }
+
+    return $form['new_blast']['query']['fld_text_fasta'];
   }
 }
