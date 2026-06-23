340a06ad19c14b8f291b825080aa980a0fccae4c
Lacey-Anne Sanderson <las166@mail.usask.ca>
2026-06-23 22:37:27 +0000

    Update entity to use attributes instead of annotations.

--- src/Entity/TripalBlastDatabase.php
+++ src/Entity/TripalBlastDatabase.php
@@ -6,49 +6,47 @@
 namespace Drupal\tripal_blast\Entity;
 
 use Drupal\Core\Config\Entity\ConfigEntityBase;
+use Drupal\Core\Entity\Attribute\ConfigEntityType;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\tripal_blast\TripalBlastDatabaseInterface;
 
-/**
- * Defines an image style configuration entity.
- *
- * @ConfigEntityType(
- *   id = "tripalblastdatabase",
- *   label = @Translation("Tripal Blast Database"),
- *   handlers = {
- *     "list_builder" = "Drupal\tripal_blast\Controller\TripalBlastDatabaseListBuilder",
- *     "form" = {
- *       "add" = "Drupal\tripal_blast\Form\TripalBlastDatabaseForm",
- *       "edit" = "Drupal\tripal_blast\Form\TripalBlastDatabaseForm",
- *       "delete" = "Drupal\tripal_blast\Form\TripalBlastDatabaseDeleteForm"
- *     }
- *   },
- *   admin_permission = "administer tripal",
- *   config_prefix = "tripal_blast",
- *   entity_keys = {
- *     "id" = "id",
- *     "name" = "name",
- *     "path" = "path",
- *     "dbtype" = "dbtype",
- *     "dbxref_id_regexp" = "dbxref_id_regexp",
- *     "dbxref_db_id" = "dbxref_db_id",
- *     "dbxref_linkout_type" = "dbxref_linkout_type",
- *   },
- *   config_export = {
- *     "id",
- *     "name",
- *     "path",
- *     "dbtype",
- *     "dbxref_id_regexp",
- *     "dbxref_db_id",
- *     "dbxref_linkout_type",
- *   },
- *   links = {
- *     "edit-form" = "/admin/tripal/extension/tripal_blast/configuration/tripalblastdatabase/edit/{tripalblastdatabase}",
- *     "delete-form" = "/admin/tripal/extension/tripal_blast/configuration/tripalblastdatabase/{tripalblastdatabase}/delete"
- *   }
- * )
- */
-class TripalBlastDatabase extends ConfigEntityBase implements TripalBlastDatabaseInterface {  
+#[ConfigEntityType(
+  id: "tripalblastdatabase",
+  label: new TranslatableMarkup("Tripal Blast Database"),
+  handlers: [
+    "list_builder" => "Drupal\tripal_blast\Controller\TripalBlastDatabaseListBuilder",
+    "form" => [
+      "add" => "Drupal\tripal_blast\Form\TripalBlastDatabaseForm",
+      "edit" => "Drupal\tripal_blast\Form\TripalBlastDatabaseForm",
+      "delete" => "Drupal\tripal_blast\Form\TripalBlastDatabaseDeleteForm",
+    ],
+  ],
+  admin_permission: "administer tripal",
+  config_prefix: "tripal_blast",
+  entity_keys: [
+    "id" => "id",
+    "name" => "name",
+    "path" => "path",
+    "dbtype" => "dbtype",
+    "dbxref_id_regexp" => "dbxref_id_regexp",
+    "dbxref_db_id" => "dbxref_db_id",
+    "dbxref_linkout_type" => "dbxref_linkout_type",
+  ],
+  config_export: [
+    "id",
+    "name",
+    "path",
+    "dbtype",
+    "dbxref_id_regexp",
+    "dbxref_db_id",
+    "dbxref_linkout_type",
+  ],
+  links: [
+    "edit-form" => "/admin/tripal/extension/tripal_blast/configuration/tripalblastdatabase/edit/{tripalblastdatabase}",
+    "delete-form" => "/admin/tripal/extension/tripal_blast/configuration/tripalblastdatabase/{tripalblastdatabase}/delete",
+  ],
+)]
+class TripalBlastDatabase extends ConfigEntityBase implements TripalBlastDatabaseInterface {
   /**
    * The primary identifier for a node.
    * @var integer
